82a55bb6d1ccabb0865ae40a02c1cd9cbef710f5
Richard Wilkes <rwilkes@fanatics.com>
2017-08-14 11:05:06 -0700

    Add support for only running one copy per workspace

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ executed, a "fast" group and a "slow" group. The fast group consists of gofmt,
 goimports, golint, ineffassign, misspell, vet. The slow group consists of
 megacheck, errchk, interfacer, unconvert, structcheck.
 
-The intent is for this to incorporate a way of ensuring only one set of
-linting is being performed at once, since IDEs like vscode can easily swamp
-the machine making it unresponsive if you have them set to run the linters on
-save and you save many files in succession. This capability has not yet been
-implemented, but will be worked on next.
+To run this from vscode, add these lines to preferences:
+
+    {
+        "go.lintTool": "dirt",
+        "go.lintFlags": ["-1"],
+        "go.lintOnSave": "workspace",
+        "go.vetOnSave": "off",
+    }
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/richardwilkes/atexit"
@@ -18,6 +21,7 @@ func main() {
 
 	timeout := 5 * time.Minute
 	fastOnly := false
+	onlyOne := false
 
 	var buffer bytes.Buffer
 	buffer.WriteString(`Run linting checks against Go code. Two groups of linters are executed, a "fast" group and a "slow" group. The fast group consists of `)
@@ -40,6 +44,7 @@ func main() {
 	cl.Description = buffer.String()
 	cl.NewDurationOption(&timeout).SetSingle('t').SetName("timeout").SetArg("duration").SetUsage("Sets the timeout. If the linters run longer than this, they will be terminated and an error will be returned")
 	cl.NewBoolOption(&fastOnly).SetSingle('f').SetName("fastonly").SetUsage("When set, only the fast linters are run")
+	cl.NewBoolOption(&onlyOne).SetSingle('1').SetName("one").SetUsage("When set, only the last started invocation for the repo will complete; any others will be terminated")
 	cl.Parse(os.Args[1:])
 
 	l, err := newLint(".", linters(fastOnly))
@@ -47,5 +52,34 @@ func main() {
 		fmt.Println(err)
 		atexit.Exit(1)
 	}
+
+	if onlyOne {
+		for {
+			lockFile := filepath.Join(l.repoPath, ".dirtlock")
+			if f, err := os.OpenFile(lockFile, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666); err == nil {
+				fmt.Fprint(f, os.Getpid())
+				if err = f.Close(); err != nil {
+					fmt.Printf("Unable to write lock file: %s\n", lockFile)
+					atexit.Exit(1)
+				}
+				atexit.Register(func() {
+					if os.Remove(lockFile) != nil {
+						fmt.Printf("Unable to remove lock file: %s\n", lockFile)
+					}
+				})
+				break
+			} else {
+				if data, err := ioutil.ReadFile(lockFile); err == nil {
+					if pid, err := strconv.Atoi(string(data)); err == nil {
+						if p, err := os.FindProcess(pid); err == nil {
+							p.Kill()
+						}
+						os.Remove(lockFile)
+					}
+				}
+			}
+		}
+	}
+
 	atexit.Exit(l.run(timeout))
 }
