7960d9b89cb10a63124cd0fdda36f22466e52fa6
Richard Wilkes <wilkes@me.com>
2018-11-12 08:29:59 -0800

    Fix comment

--- linter.go
+++ linter.go
@@ -26,7 +26,7 @@ var (
 		{cmd: "misspell", args: []string{"-locale", "US", FILES}, pkg: "github.com/client9/misspell/cmd/misspell"},
 		{cmd: "go", args: []string{"tool", "vet", "-all", "-shadow", DIRS}},
 	}
-	// SlowLinters holds the linters that are known to execute quickly.
+	// SlowLinters holds the linters that are known to execute slowly.
 	SlowLinters = []linter{
 		{cmd: "megacheck", args: []string{PKGS}, pkg: "honnef.co/go/tools/cmd/megacheck"},
 		{cmd: "errchk", args: []string{"-abspath", "-blank", "-asserts", "-ignore", "github.com/richardwilkes/errs:Append", "-ignore", "github.com/richardwilkes/toolbox/errs:Append", "-ignore", "io:CloseWithError", PKGS}, pkg: "github.com/richardwilkes/errchk"},
